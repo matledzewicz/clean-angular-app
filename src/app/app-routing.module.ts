2858b7336f82a665d0871f23eed1fd802cd2ab11
Mateusz Łędzewicz <mat.ledzewicz@gmail.com>
2023-05-05 10:46:57 +0200

    feat: simple list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'products', component: ProductListComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), ProductListComponentModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/product-list/product-list.component-module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ProductListComponent],
+  providers: [],
+  exports: [ProductListComponent]
+})
+export class ProductListComponentModule {
+}
